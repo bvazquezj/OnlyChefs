0ccb12b2d829669f14d25a46561f9da94cc8e108
Brandon Vazquez <bvazquezj1700@gmail.com>
2024-11-11 04:42:33 -0600

    Adding recipes page and curses page whit data

---
+++ FrontOnlyChefs/src/data/allCurseData.js
@@ -0,0 +1,164 @@
+const allCursos = [
+    {
+        image: "https://images.unsplash.com/photo-1601000937859-3031d1be1caa?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Curso de Reposteria tradicional",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1646026105549-bc1dc2f4e552?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGdhbGxldGFzfGVufDB8fDB8fHww",
+        title: "Aprende a hacer galletas",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1532336414038-cf19250c5757?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZXNwZWNpYXN8ZW58MHx8MHx8fDA%3D",
+        title: "Domina las especias",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://plus.unsplash.com/premium_photo-1694547924505-caf71944b4df?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Q29jaW5hJTIwSmFwb25lc2F8ZW58MHx8MHx8fDA%3D",
+        title: "Curso de Cocina Japonesa",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1601000937859-3031d1be1caa?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Curso de Reposteria tradicional",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1646026105549-bc1dc2f4e552?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGdhbGxldGFzfGVufDB8fDB8fHww",
+        title: "Aprende a hacer galletas",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1532336414038-cf19250c5757?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZXNwZWNpYXN8ZW58MHx8MHx8fDA%3D",
+        title: "Domina las especias",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://plus.unsplash.com/premium_photo-1694547924505-caf71944b4df?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Q29jaW5hJTIwSmFwb25lc2F8ZW58MHx8MHx8fDA%3D",
+        title: "Curso de Cocina Japonesa",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1601000937859-3031d1be1caa?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Curso de Reposteria tradicional",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1646026105549-bc1dc2f4e552?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGdhbGxldGFzfGVufDB8fDB8fHww",
+        title: "Aprende a hacer galletas",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1532336414038-cf19250c5757?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZXNwZWNpYXN8ZW58MHx8MHx8fDA%3D",
+        title: "Domina las especias",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://plus.unsplash.com/premium_photo-1694547924505-caf71944b4df?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Q29jaW5hJTIwSmFwb25lc2F8ZW58MHx8MHx8fDA%3D",
+        title: "Curso de Cocina Japonesa",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1601000937859-3031d1be1caa?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Curso de Reposteria tradicional",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1646026105549-bc1dc2f4e552?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGdhbGxldGFzfGVufDB8fDB8fHww",
+        title: "Aprende a hacer galletas",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1532336414038-cf19250c5757?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZXNwZWNpYXN8ZW58MHx8MHx8fDA%3D",
+        title: "Domina las especias",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://plus.unsplash.com/premium_photo-1694547924505-caf71944b4df?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Q29jaW5hJTIwSmFwb25lc2F8ZW58MHx8MHx8fDA%3D",
+        title: "Curso de Cocina Japonesa",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1601000937859-3031d1be1caa?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Curso de Reposteria tradicional",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1646026105549-bc1dc2f4e552?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGdhbGxldGFzfGVufDB8fDB8fHww",
+        title: "Aprende a hacer galletas",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1532336414038-cf19250c5757?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZXNwZWNpYXN8ZW58MHx8MHx8fDA%3D",
+        title: "Domina las especias",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://plus.unsplash.com/premium_photo-1694547924505-caf71944b4df?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Q29jaW5hJTIwSmFwb25lc2F8ZW58MHx8MHx8fDA%3D",
+        title: "Curso de Cocina Japonesa",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    }
+]
+
+export default allCursos;
---
+++ FrontOnlyChefs/src/data/dataRecipe.js
@@ -0,0 +1,36 @@
+const recipes = [
+    {
+        image: "https://images.unsplash.com/photo-1509440159596-0249088772ff?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Receta Pan Artesanal ",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://plus.unsplash.com/premium_photo-1674511582428-58ce834ce172?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZXNwYWdldGlpc3xlbnwwfHwwfHx8MA%3D%3D",
+        title: "Cocina los espaguetis perfectos",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1487124504955-e42a39e11aaf?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bXVmZmlufGVufDB8fDB8fHww",
+        title: "Cocina estos ricos Muffins",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    },
+    {
+        image: "https://plus.unsplash.com/premium_photo-1671559021023-3da68c12aeed?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bGFzYSVDMyVCMWF8ZW58MHx8MHx8fDA%3D",
+        title: "Receta de Lasaña de carne",
+        author: "Chef Mario Rossi",
+        rating: 4.7,
+        reviews: 500,
+        price: "$19.99"
+    }
+]
+
+export default recipes;
